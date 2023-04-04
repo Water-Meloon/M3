84429ca9701f10270003443ad480d3dc42acadbd
JackEdwards11 <jackmedwards@gmail.com>
2023-04-04 11:10:48 -0400

    j

--- src/components/MyToDoList/MyToDoList.js
+++ src/components/MyToDoList/MyToDoList.js
@@ -1,18 +1,44 @@
 import Task from "../Classes/Task";
 import React, { useState, useEffect } from "react";
 
+const tasks = [
+  {
+    key: "t1",
+    name: "Groceries",
+    topic: "Shopping",
+    dueDate: "04/16/2023",
+    taskStatus: "incomplete",
+  },
+  {
+    key: "t2",
+    name: "Groceries",
+    topic: "Shopping",
+    dueDate: "04/16/2023",
+    taskStatus: "incomplete",
+  },
+];
+
 const MyToDoList = () => {
+  console.log(typeof tasks);
+  const [data, setData] = useState(tasks);
 
-  const [data, setData] = useState();
+  // FIX
+  //   useEffect(() => {
+  //     fetch('../data/tasks.json')
+  //       .then((response) => response.json())
+  //       .then((data) => setData(data.message))
+  //       .catch((error) => console.log(error));
+  //   }, []);
+  console.log(typeof data);
+  //
 
-  useEffect(() => {
-    fetch('../data/tasks.json')
-      .then((response) => response.json())
-      .then((data) => setData(data.message))
-      .catch((error) => console.log(error));
-  }, []);
-
-  return <div></div>;
+  return (
+    <div>
+      {data.map((element) => (
+        <div>{element.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default MyToDoList;
