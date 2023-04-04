953922b70ce8d4cfff79b0824a8139ee042ea618
JackEdwards11 <jackmedwards@gmail.com>
2023-04-03 20:34:17 -0400

    j

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,15 +1,17 @@
 import Login from "../Home/Login";
-import classes from './SignUp.module.css';
+import classes from "./SignUp.module.css";
 
 const SignUp = (props) => {
   const loginHandler = () => {
     props.login();
-    props.changePage('Home');
+    props.changePage("Home");
   };
 
   return (
     <>
-    {props.isLoggedIn && <h1 className={classes.h1}>You are already logged in!</h1>}
+      {props.isLoggedIn && (
+        <h1 className={classes.h1}>You are already logged in!</h1>
+      )}
       {!props.isLoggedIn && (
         <Login
           isLoggedIn={props.isLoggedIn}
