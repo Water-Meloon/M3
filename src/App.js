00d2d1b9b364adcef64199232bd021a64118ff96
JackEdwards11 <jackmedwards@gmail.com>
2023-04-03 20:14:53 -0400

    j

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import classes from "./App.module.css";
 import CreateNewTask from "./components/CreateNewTask/CreateNewTask";
 import Home from "./components/Home/Home";
 import MyToDoList from "./components/MyToDoList/MyToDoList";
@@ -9,6 +9,7 @@ import Nav from "./components/UI/Nav";
 import ViewTaskDetail from "./components/ViewTaskDetail/ViewTaskDetail";
 import Card from "./components/UI/Card";
 import Logout from "./components/UI/Logout";
+import Footer from "./components/UI/Footer";
 
 function App() {
   const [pageName, setPageName] = useState("Home");
@@ -40,18 +41,21 @@ function App() {
   };
 
   return (
-    <>
+    <div className={classes.wrapper}>
       <Header pageName={pageName} />
       <Nav changePage={pageChangeHandler} />
       <Card>
-        {pageName === "Home" && <Home isLoggedIn={isLoggedIn} login={loginHandler}/>}
+        {pageName === "Home" && (
+          <Home isLoggedIn={isLoggedIn} login={loginHandler} />
+        )}
         {pageName === "My To Do List" && <MyToDoList />}
         {pageName === "Create New Task" && <CreateNewTask />}
         {pageName === "View Task Detail" && <ViewTaskDetail />}
         {pageName === "Sign Up" && <SignUp />}
         {isLoggedIn && <Logout logout={logoutHandler} />}
       </Card>
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/UI/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.footer}>Milestone 2 Group: 9</div>
+    </div>
+  );
+};
+
+export default Footer;
